e4cca6f5276ac59ca4023aade232a9832c4281d5
zen <anshenmesheng@hotmail.com>
2024-03-19 21:33:18 +0800

    factory

---
+++ src/factory.rs
@@ -0,0 +1,28 @@
+// factory
+
+
+pub struct FactoryClass {
+} 
+
+pub struct ComplicatedClass {
+    field: String,
+}
+
+impl FactoryClass {
+    pub fn new() -> Self {
+        FactoryClass{}
+    }
+
+    pub fn create_complicated_instance(self) -> ComplicatedClass {
+        // some really complicated procedure to create ComplicatedClass instance
+        ComplicatedClass{
+            field: String::from("test"),
+        }
+    }
+}
+
+impl ComplicatedClass {
+    pub fn print(self) {
+        print!("{:?}", self.field);
+    } 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod singleton;
+mod factory;
+
+use factory::FactoryClass;
 
 fn main() {
     println!("Hello, world!");
 
     let sc = singleton::get_instance();
     sc.print();
+
+
+    let fact = FactoryClass::new();
+    let target = fact.create_complicated_instance();
+    target.print();
 }
